fb85dc8afeb907811fb6337fdc831b269141764f
TK Gospodinov <tk.gospodinov@cerner.com>
2012-06-26 10:56:25 -0500

    Changed code for testing

--- lib/omniauth/strategies/fitbit.rb
+++ lib/omniauth/strategies/fitbit.rb
@@ -15,12 +15,10 @@ module OmniAuth
       }
 
       uid do
-        puts ">>>>>>>>>>>>>>>>>>>>>>>>> PARAMS: #{access_token.params['encoded_user_id']}"
         access_token.params['encoded_user_id']
       end
 
       info do
-        puts "DISPLAY NAME >>>>>>>>>>>>>>>>>>>> #{raw_info["user"]["fullName"]}"
         {
             :display_name => raw_info["user"]["fullName"]
         }
@@ -33,8 +31,6 @@ module OmniAuth
       end
 
       def raw_info
-        #@raw_info ||= MultiJson.load(access_token.get("http://api.fitbit.com/1/user/-/profile.json").body)
-        puts ">>>>>>>>>>>>>>>>>>>>>>>>>>> RAW INFO: #{@raw_info.to_s}"
         @raw_info ||= MultiJson.load(access_token.get("http://api.fitbit.com/1/user/-/profile.json").body)
       end
     end
