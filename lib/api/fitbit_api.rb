6c6a36aa8143547e7283382047a20891275c75b4
scrawlon <scrawlon@gmail.com>
2013-08-29 20:25:09 +0000

    Basic API module working. First Fitbit API method specs passing.

--- lib/api/fitbit_api.rb
+++ lib/api/fitbit_api.rb
@@ -1,5 +1,47 @@
 module Fitbit
   class Api < OmniAuth::Strategies::Fitbit
+    
+    def api_call consumer_key, consumer_secret, params, auth_token="", auth_secret=""
+      request = build_request(consumer_key, consumer_secret, auth_token, auth_secret)
+      request_url = build_url(@@api_version, params)
+      request_http_method = get_http_method(params['api-method'])
+      request.request( request_http_method,  "http://api.fitbit.com#{request_url}" )
+    end
+
+    def build_url api_version, params
+      api_method = get_api_method(params['api-method'])
+      api_format = params['response-format']
+      api_query = uri_encode_query(params['query']) if params['query']
+      api_query ||= ""
+      request_url = "/#{api_version}/#{api_method}.#{api_format}#{api_query}"
+    end
+
+    private 
+
+    def get_api_method method
+      api_method = @@fitbit_methods["#{method}"]['resources']
+      api_method_url = api_method.join("/")
+    end
+
+    def uri_encode_query query
+      api_query = OAuth::Helper.normalize({ 'query' => query })
+      "?#{api_query}"
+    end
+
+    def build_request consumer_key, consumer_secret, auth_token, auth_secret
+      fitbit = Fitbit::Api.new :fitbit, consumer_key, consumer_secret
+      access_token = OAuth::AccessToken.new fitbit.consumer, auth_token, auth_secret
+    end
+    
+    def get_http_method method
+      api_http_method = @@fitbit_methods["#{method}"]['http_method']
+    end
+
+    @@api_version = 1
+
+    @@fitbit_methods = {
+      'API-Search-Foods' => { 'http_method' => 'get', 'resources' => ['foods', 'search'] }
+    }
 
   end
 end
--- spec/api/fitbit_api_spec.rb
+++ spec/api/fitbit_api_spec.rb
@@ -1,5 +1,36 @@
 require 'spec_helper'
 
 describe Fitbit::Api do
+  subject do
+    Fitbit::Api.new({})
+  end
 
+  before(:all) do
+    @consumer_key = 'user_consumer_key'
+    @consumer_secret = 'user_consumer_secret'
+    @api_version = 1
+  end
+
+  context 'API-Search-Foods method' do
+    before(:each) do
+      @params = { 
+        'api-method'      => 'API-Search-Foods',
+        'response-format' => 'xml',
+        'query'           => 'banana cream pie'
+      }
+    end
+
+    it 'should create API-Search-Foods url' do
+      expect(subject.build_url(@api_version, @params)).to eq('/1/foods/search.xml?query=banana%20cream%20pie')
+    end
+
+    it 'should create API-Search-Foods OAuth request' do
+      api_search_foods_url = subject.build_url(@api_version, @params)
+      stub_request(:get, "api.fitbit.com#{api_search_foods_url}")
+      api_call = subject.api_call(@consumer_key, @consumer_secret, @params)
+      expect(api_call.class).to eq(Net::HTTPOK)
+    end
+
+  end
+    
 end
