83dc63cb8afa8906b56240d8ed65d87a07fd0cbe
scrawlon <scrawlon@gmail.com>
2013-09-06 07:07:11 +0000

    Delete-Heart-Rate-Log specs pass.

--- spec/api/fitbit_api_spec.rb
+++ spec/api/fitbit_api_spec.rb
@@ -524,6 +524,32 @@ describe Fitbit::Api do
     end
   end
 
+  context 'API-Delete-Sleep-Log method' do
+    before(:each) do
+      @api_method = 'api-delete-sleep-log'
+      @api_url = '/1/user/-/sleep/12345.xml'
+      @params = {
+        'api-method'      => 'API-Delete-Sleep-Log',
+        'sleep-log-id'         => '12345'
+      }
+    end
+
+    it 'should create API-Delete-Sleep-Log url' do
+      expect(subject.build_url(@api_version, @params)).to eq(@api_url)
+    end
+
+    it 'should create API-Delete-Sleep-Log OAuth request' do
+      stub_request(:delete, "api.fitbit.com#{@api_url}")
+      api_call = subject.api_call(@consumer_key, @consumer_secret, @params, @auth_token, @auth_secret)
+      expect(api_call.class).to eq(Net::HTTPOK)
+    end
+
+    it 'should return a helpful error if auth_tokens are missing' do
+      error_message = "#{@api_method} requires user auth_token and auth_secret."
+      lambda { subject.api_call(@consumer_key, @consumer_secret, @params) }.should raise_error(RuntimeError, error_message)
+    end
+  end
+
 
   context 'API-Search-Foods method' do
     before(:each) do
