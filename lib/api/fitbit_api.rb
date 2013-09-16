a4319e61a30ca46fbaae5d4c2be44d28e4ffa66b
scrawlon <scrawlon@gmail.com>
2013-09-16 23:09:43 +0000

    'ignoring wrong elements is deprecated error' appears to orginate in get_lowercase_api_method.

--- lib/api/fitbit_api.rb
+++ lib/api/fitbit_api.rb
@@ -51,7 +51,7 @@ module Fitbit
 
     def get_lowercase_api_method params
       api_strings = Hash[params.map { |k,v| [k, v.downcase] if k == 'api-method' }]
-      api_parameters_and_headers = Hash[params.map { |k,v| [k, v] if k != 'api-method' }]
+      api_parameters_and_headers = Hash[params.map { |k,v| [k, v] if !k.nil? && k != 'api-method' }]
       api_strings.merge(api_parameters_and_headers)
     end
 
